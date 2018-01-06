cf4c49cd0ff399d85e03199023d09a2151e03b75
bris98 <1716110397@utec-tgo.edu.mx>
2018-01-05 22:44:36 -0600

    proyecto

--- src/controllers/ControllerAsistencias.java
+++ src/controllers/ControllerAsistencias.java
@@ -38,15 +38,61 @@ public final class ControllerAsistencias  implements FocusListener {
          
      }
      public void setValores(){
-         //jtf 
-     }
+    public void fechas() {
+             try{
+     int as = Integer.parseInt(jtf_id_cliente.getText());
+    String sql = "SELECT MEMBRESIA.FECHA_FINAL,CLIENTE.NOMBRE from MEMBRESIA  INNER JOIN CLIENTE ON MEMBRESIA.ID_CLIENTE = CLIENTE.ID_CLIENTE WHERE MEMBRESIA.ID_CLIENTE = "+as;
+    System.out.println(sql);
+    rs=st.executeQuery(sql);
+    rs.first();
+    this.jtf_fecha_vencimiento.setText(rs.getString("FECHA_FINAL"));
 
-    @Override
-    public void focusGained(FocusEvent e) {
-    }
+    String sqll = "select * from ASISTENCIA where ID_CLIENTE ='" + as+"'";
+    System.out.println(sqll);
+    rs=st.executeQuery(sqll);
+    rs.first();
+    this.jtf_fecha_actual.setText(rs.getString("FECHA_ASISTENCIA"));
+    
+    String Fecha_Vencimiento =jtf_fecha_vencimiento.getText();
+    String Fecha_Actual = jtf_fecha_actual.getText();
+    
+    JOptionPane.showMessageDialog(null, "FAVOR DE RENOVAR TU MEMBRESIA");
 
-    @Override
-    public void focusLost(FocusEvent e) {
-    }
+    if (Fecha_Vencimiento.equals(Fecha_Actual)) {
+            System.out.println("WORKING");
+            JOptionPane.showMessageDialog(null, "FAVOR DE RENOVAR TU MEMBRESIA");
 
+}
+             } catch (SQLException err) {
+            JOptionPane.showMessageDialog(null, "Error " + err.getMessage());
+        }
+}
+    public void fechafinal() {
+    try{
+     int as = Integer.parseInt(jtf_id_cliente.getText());
+    String sql = "select * from MEMBRESIA ";
+    System.out.println(sql);
+    rs=st.executeQuery(sql);
+    rs.first();
+    this.jtf_fecha_vencimiento.setText(rs.getString("FECHA_FINAL"));
+
+    String sqll = "select * from ASISTENCIA where ID_CLIENTE ='" + as+"'";;
+    System.out.println(sqll);
+    rs=st.executeQuery(sqll);
+    rs.first();
+    this.jtf_fecha_actual.setText(rs.getString("FECHA_ASISTENCIA"));
+    
+    String Fecha_Vencimiento =jtf_fecha_vencimiento.getText();
+    String Fecha_Actual = jtf_fecha_actual.getText();
+    
+    JOptionPane.showMessageDialog(null, "FAVOR DE RENOVAR TU MEMBRESIA");
+
+    if (Fecha_Vencimiento.equals(Fecha_Actual)) {
+            System.out.println("WORKING");
+            JOptionPane.showMessageDialog(null, "FAVOR DE RENOVAR TU MEMBRESIA");
+
+}
+             } catch (SQLException err) {
+            JOptionPane.showMessageDialog(null, "Error " + err.getMessage());
+        }
 }
